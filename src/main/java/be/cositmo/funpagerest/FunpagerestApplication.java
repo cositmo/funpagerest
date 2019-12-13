87107a144672d64eaef7dd38bd29fee3d2069d09
cositmo <hans.corsmit@telenet.be>
2019-12-13 14:05:51 +0100

    Base Setup

---
+++ src/main/java/be/cositmo/funpagerest/FunpagerestApplication.java
@@ -0,0 +1,13 @@
+package be.cositmo.funpagerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunpagerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunpagerestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/cositmo/funpagerest/FunpagerestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cositmo.funpagerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunpagerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
